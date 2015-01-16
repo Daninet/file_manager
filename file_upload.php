dc55bce4bae8869f3f1b53b0326f6f572c7dbe10
Dani Biró <danibiro93@gmail.com>
2015-01-16 17:06:52 +0200

    Adding UIImage

--- file_upload.php
+++ file_upload.php
@@ -444,6 +444,33 @@ class UIIcon extends UIElement
 	}
 }
 
+class UIImage extends UIElement
+{
+	protected $href = '';
+	protected $image = '';
+
+	public function __construct($image, $href = '') {
+		$this->image = $image;
+		$this->href = $href;
+
+		return $this;
+	}
+
+	public function renderHTML() {
+		if($this->href != '') 
+			$this->html .= '<a href="'.$this->href.'">';
+
+		$this->html .= '<img src="'.$this->image.'"'.$this->getHTMLClass().$this->getHTMLID().' />';
+
+		
+		if($this->href != '') 
+			$this->html .= '</a>';
+
+		return $this->html;
+
+	}
+}
+
 class UITable extends UIElement
 {
 	private $columns = array();
